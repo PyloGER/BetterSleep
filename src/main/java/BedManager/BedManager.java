36ad7a148c4fdd69553c856c0598db9a4d2db075
PyloGER <81117842+PyloGER@users.noreply.github.com>
2021-07-26 13:05:26 +0200

    Initial commit

---
+++ src/main/java/BedManager/BedManager.java
@@ -0,0 +1,25 @@
+package BedManager;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerBedEnterEvent;
+
+public class BedManager implements Listener {
+
+    public static String prefix = "§8[§b§lSystem§8§l] ";
+
+    @EventHandler
+    public void onEnterBed(PlayerBedEnterEvent event) {
+        Player player = event.getPlayer();
+        if (event.getBedEnterResult() == PlayerBedEnterEvent.BedEnterResult.OK) {
+            player.sendMessage(prefix + "§7Gute Nacht §b§l"+ player.getDisplayName() + "§7 bis morgen§8.");
+            Bukkit.broadcastMessage(prefix + "§7Der Spieler §b§l"+ player.getDisplayName() + "§7 hat geschlafen§8.");
+            player.getWorld().setTime(0);
+
+        } else {
+            Bukkit.broadcastMessage(prefix + "§cDu kann derzeit nicht schlafen§8.");
+        }
+    }
+}
---
+++ src/main/java/de/pyloger/bettersleep/PyloGER.java
@@ -0,0 +1,28 @@
+package de.pyloger.bettersleep;
+
+import BedManager.BedManager;
+import org.bukkit.Bukkit;
+import org.bukkit.block.data.type.Bed;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PyloGER extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        Bukkit.getPluginManager().registerEvents(new BedManager(), this);
+
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    public void Data(){
+
+        String prefix = "§8[§b§lSystem§8§l] ";
+
+    }
+}
